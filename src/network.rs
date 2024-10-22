c51f9febf49fd85ed677c67d9121bef2f8ee3ae5
Yehor Iliushchenko <buaueru@gmail.com>
2024-10-22 20:09:36 +0300

    Changed broadcast address

--- src/network.rs
+++ src/network.rs
@@ -37,7 +37,7 @@ fn ping_broadcast_channel(socket: UdpSocket) -> Result<(), Error> {
 
 pub fn listen_to_broadcast_address() {
     let listen_thread = thread::spawn(|| {
-        let socket: UdpSocket = UdpSocket::bind("255.255.255.255:0").unwrap();
+        let socket: UdpSocket = UdpSocket::bind("0.0.0.0:0").unwrap();
         let connection_timeout = Some(Duration::new(5, 0));
         socket.set_broadcast(true).unwrap();
         socket.set_read_timeout(connection_timeout).unwrap();
